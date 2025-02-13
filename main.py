9d0b5d54806d71278d78f48fc0556da54cbff41a
zhalgaskhan <zhalgas.abylkasym@gmail.com>
2025-02-13 23:39:34 +0500

    third commit

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-# Mount the "static" directory to serve frontend assets
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
